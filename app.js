7fa9b275b3c733078543b637e159adc20f14197d
deepikad24 <deepikadevops24@gmail.com>
2025-04-24 15:45:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Deepika is saying hi!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
